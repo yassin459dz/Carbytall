65444d5227e47889c5e55afa89aaa16b943d2140
Roua459 <yassinsina458@gmail.com>
2025-02-16 00:05:09 +0100

    s

--- resources/views/livewire/Matricule/matricule.blade.php
+++ resources/views/livewire/Matricule/matricule.blade.php
@@ -45,7 +45,7 @@
     <div>
         <div>
             <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-                Create Brand
+                Create Matricule
             </button>
         </div>
         <div wire:ignore>
